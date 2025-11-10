1fc6ebcd76a6171a2631d64ecf50f8cfb3ab1e63
ANDRES VISCO BONOMO <andres.bonomo@trend-it.com.mx>
2025-11-10 02:36:58 +0000

    post

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import logging
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello():
     app.logger.info('ok function')
     return 'Hello, World!'
+
+@app.route('/test-avisco', methods=['POST'])
+def test_avisco():
+    app.logger.info('Received POST on /test-avisco')
+    # Puedes acceder a los datos con request.form, request.json, etc. si lo necesitas.
+    return jsonify({"message": "Aviso recibido", "status": "success"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
